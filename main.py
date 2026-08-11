6e8ad5fee65ea8435c5fd41b190a6c13e308e1f2
digiteng <digiteng@gmail.com>
2026-08-11 14:10:53 +0300

    Fix environment variable name for INSTA_URL

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import niquests
 app = FastAPI()
 
 MAC_X = os.getenv("MAC_0", "")
-INSTA_URL = os.getenv("insta_url", "")
+INSTA_URL = os.getenv("INSTA_URL", "")
 HEADERS = {
     'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 12_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Instagram 105.0.0.11.118 (iPhone11,8; iOS 12_3_1; en_US; en-US; scale=2.00; 828x1792; 165586599)',
     'x-ig-app-id': '936619743392459',
