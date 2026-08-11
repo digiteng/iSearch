8850c82feb080df54913cb317965ba300b27b42b
digiteng <digiteng@gmail.com>
2026-08-11 13:21:21 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import niquests
 
 app = FastAPI()
 
-VERCEL_API_KEY = os.getenv("MY_APP_PASSWORD", "")
+VERCEL_API_KEY = os.getenv("MAC_0", "")
 INSTA_URL = os.getenv("insta_url", "")
 HEADERS = {
     'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 12_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Instagram 105.0.0.11.118 (iPhone11,8; iOS 12_3_1; en_US; en-US; scale=2.00; 828x1792; 165586599)',
