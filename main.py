b89991b44402c8e56cb44d75f36321a8e84be50b
digiteng <digiteng@gmail.com>
2026-08-11 04:29:41 +0300

    Refactor Instagram cookie handling in main.py
    Commented out INSTAGRAM_COOKIE and its usage in HEADERS.

--- main.py
+++ main.py
@@ -1,14 +1,14 @@
 from fastapi import FastAPI, HTTPException
 import niquests
 import uvicorn
-INSTAGRAM_COOKIE = os.getenv("INSTAGRAM_COOKIE", "")
+#INSTAGRAM_COOKIE = os.getenv("INSTAGRAM_COOKIE", "")
 
 app = FastAPI()
 
 HEADERS = {
 	'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 12_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Instagram 105.0.0.11.118 (iPhone11,8; iOS 12_3_1; en_US; en-US; scale=2.00; 828x1792; 165586599)',
 	'x-ig-app-id': '936619743392459',
-	'cookie': INSTAGRAM_COOKIE,
+	#'cookie': INSTAGRAM_COOKIE,
 }
 
 @app.get("/")
