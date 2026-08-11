876381eafef8d943d4e676ca235070dbae34ad2f
digiteng <digiteng@gmail.com>
2026-08-11 14:06:28 +0300

    Update main.py

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import niquests
 
 app = FastAPI()
 
-VERCEL_API_KEY = os.getenv("MAC_0", "")
+MAC_X = os.getenv("MAC_0", "")
 INSTA_URL = os.getenv("insta_url", "")
 HEADERS = {
     'user-agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 12_3_1 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148 Instagram 105.0.0.11.118 (iPhone11,8; iOS 12_3_1; en_US; en-US; scale=2.00; 828x1792; 165586599)',
@@ -17,25 +17,19 @@ def read_root():
 
 @app.get("/search")
 def search(user: str, x_api_key: str = Header(None)):
-
-    if not x_api_key or x_api_key != VERCEL_API_KEY:
+    if not x_api_key or x_api_key != MAC_X:
         raise HTTPException(status_code=401, detail="Unauthorized access! Invalid API token.")
-
     if not user:
         raise HTTPException(status_code=400, detail="No username was specified.")
     url = f"{INSTA_URL}{user}"
     try:
         r = niquests.get(url, headers=HEADERS)
-
         if r.status_code != 200:
             raise HTTPException(status_code=r.status_code, detail=f"Instagram API error: {r.status_code}")
-            
         instagram_data = r.json()
         user_info = instagram_data.get("data", {}).get("user", {})
-        
         if not user_info:
-            return {"message: User not found or profile is private."}
-
+            return {"message": "User not found or profile is private."}
         clean_profile = {
             "user_name": user_info.get("username"),
             "full_name": user_info.get("full_name"),
@@ -48,18 +42,21 @@ def search(user: str, x_api_key: str = Header(None)):
 
         reels_list = []
         posts = user_info.get("edge_owner_to_timeline_media", {}).get("edges", [])
-        
+
         for post in posts:
             node = post.get("node", {})
             if node.get("__typename") == "GraphVideo" or node.get("is_video") is True:
+                caption_edges = node.get("edge_media_to_caption", {}).get("edges", [])
+                description = caption_edges[0].get("node", {}).get("text", "") if caption_edges else ""
+
                 video_info = {
                     "id": node.get("id"),
                     "shortcode_id": node.get("shortcode"),
                     "url": f"https://instagram.com{node.get('shortcode')}/",
-                    "video_url": node.get("video_url"), # Doğrudan mp4 linki
+                    "video_url": node.get("video_url"), 
                     "view_count": node.get("video_view_count", 0),
                     "liked": node.get("edge_liked_by", {}).get("count", 0),
-                    "desc": node.get("edge_media_to_caption", {}).get("edges", [{}])[0].get("node", {}).get("text", "") if node.get("edge_media_to_caption", {}).get("edges") else "",
+                    "desc": description,
                     "thumbnail": node.get("display_url")
                 }
                 reels_list.append(video_info)
